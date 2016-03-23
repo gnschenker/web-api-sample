210bec6e4284a47adbc7f275f5de38a57e4cc1f3
Gabriel N. Schenker <gnschenker@gmail.com>
2016-03-23 15:19:04 -0500

    Initial commit

---
+++ Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectsController : Controller
+    {
+        private readonly Project[] _projects = new Project[]
+        {
+            new Project {id = 1, name = "Pears"},
+            new Project {id = 2, name = "Apples"},
+            new Project {id = 3, name = "Oranges"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Project> Get()
+        {
+            return _projects;
+        }
+
+        [HttpGet("{id}")]
+        public Project Get(int id)
+        {
+            return _projects.SingleOrDefault(x => x.id == id);
+        }
+    }
+
+    public class Project
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
